3516e5dc356b99f0c74477276a92f5329243e97a
fullStack20259 <jaind797@gmail.com>
2025-10-05 12:59:02 +0530

    Create countInversion.py

---
+++ countInversion.py
@@ -0,0 +1,47 @@
+def count_inversions(arr):
+    def merge_count_split_inv(arr, temp_arr, left, right):
+        if left == right:
+            return 0
+        mid = (left + right) // 2
+        inv_count = merge_count_split_inv(arr, temp_arr, left, mid)
+        inv_count += merge_count_split_inv(arr, temp_arr, mid + 1, right)
+        inv_count += merge_and_count(arr, temp_arr, left, mid, right)
+        return inv_count
+    
+    def merge_and_count(arr, temp_arr, left, mid, right):
+        i = left    # Starting index for left subarray
+        j = mid + 1 # Starting index for right subarray
+        k = left    # Starting index to be sorted
+        inv_count = 0
+
+        while i <= mid and j <= right:
+            if arr[i] <= arr[j]:
+                temp_arr[k] = arr[i]
+                i += 1
+            else:
+                temp_arr[k] = arr[j]
+                inv_count += (mid-i + 1)
+                j += 1
+            k += 1
+
+        while i <= mid:
+            temp_arr[k] = arr[i]
+            i += 1
+            k += 1
+
+        while j <= right:
+            temp_arr[k] = arr[j]
+            j += 1
+            k += 1
+
+        for i in range(left, right + 1):
+            arr[i] = temp_arr[i]
+        
+        return inv_count
+
+    n = len(arr)
+    temp_arr = [0] * n
+    return merge_count_split_inv(arr, temp_arr, 0, n - 1)
+
+arr = [2, 4, 1, 3, 5]
+print(count_inversions(arr))
